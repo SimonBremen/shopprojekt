59bf26e96b36f528a9078462c90a6aa8bf5586c9
SimonBremen <privat@simonankele.de>
2014-04-24 10:13:39 +0200

    Erster Versuch

---
+++ Testshop/src/servlets/Hauptseite.java
@@ -0,0 +1,54 @@
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Hauptseite
+ */
+@WebServlet("/Hauptseite")
+public class Hauptseite extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Hauptseite() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// Einbindung der Kopfzeile
+		RequestDispatcher rd; 
+		rd = getServletContext().getRequestDispatcher("/Kopfzeile"); 
+		rd.include(request, response); 
+		
+		// Content der Hauptseite
+		response.setContentType("text/html"); 
+		PrintWriter out = response.getWriter(); 
+		out.println("Hauptseite"); 
+		
+		// Einbindung der Fusszeile
+		rd = getServletContext().getRequestDispatcher("/Fusszeile"); 
+		rd.include(request, response); 
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
---
+++ Testshop/src/servlets/Kopfzeile.java
@@ -0,0 +1,60 @@
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Kopfzeile
+ */
+@WebServlet("/Kopfzeile")
+public class Kopfzeile extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Kopfzeile() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		// Content der Kopfzeile
+		response.setContentType("text/html"); 
+		PrintWriter out = response.getWriter(); 
+		// Tabellengenerierung
+		out.println("<table>\n<tr>\n<td>");
+		// Content Kopfzeile
+		out.println("Kopfzeile");
+		// Tabellengenerierung
+		out.println("</td>\n</tr>\n<tr>\n<td>");
+				
+		// Einbindung der Navigation
+		RequestDispatcher rd; 
+		rd = getServletContext().getRequestDispatcher("/Navigation"); 
+		rd.include(request, response); 
+		
+		// Content der Kopfzeile
+		out.println("</td>\n<td>");
+		 
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
